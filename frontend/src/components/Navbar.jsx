c1c9cd02029edb84381ee4536aeaf8fd8ebf568e
Abraham Perez <156040006+AbeDaDev@users.noreply.github.com>
2025-11-17 15:10:44 -0800

    issue with localhost

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -57,10 +57,14 @@ const Navbar = () => {
             {/* Sidebar menu for small screens */}
             <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
                 <div className='flex flex-col text-gray-600'>
-                    <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+                    <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3' cursor-pointer>
                         <img className='h-4 rotate-180' src={assets.dropdown_icon} alt=""/>
                         <p>Back</p>
                     </div>
+                    <Navlink className='py-2 pl-6 border' to='/'>HOME</Navlink>
+                    <Navlink className='py-2 pl-6 border' to='/collection'>COLLECTION</Navlink>
+                    <Navlink className='py-2 pl-6 border' to='/about'>ABOUT</Navlink>
+                    <Navlink className='py-2 pl-6 border' to='/contact'>CONTACT</Navlink>
                 </div>
             </div>
     </div>    
